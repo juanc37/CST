62d0836dfde9083f4aba956e21f6fc06bd5f78b0
Juan <juan@dreamvent.com>
2017-05-27 17:45:38 -0700

    updated password field

--- templates/httpserv.go
+++ templates/httpserv.go
@@ -11,6 +11,7 @@ import (
 type User struct {
 	ID string `json:"id"`
 	Email string `json:"email"`
+	EncrPass string `json:"password"`
 	Firstname string `json:"firstname"`
 	Lastname string `json:"lastname"`
 	Location *Location `json:"location"`
@@ -56,8 +57,8 @@ func deleteUserEndpoint(w http.ResponseWriter, req *http.Request) {
 func main() {
 	fmt.Print("running")
 	router := mux.NewRouter()
-	users = append(users, User{ID: "1", Email: "jc3fake@gmail.com", Firstname: "Juan", Lastname: "C3", Location: &Location{State: "CA", Zip: "92129"}})
-	users = append(users, User{ID: "2", Email: "zakydfake@gmail.com", Firstname: "Zaky", Lastname: "Demnianiek"})
+	users = append(users, User{ID: "1", EncrPass: "secret", Email: "jc3fake@gmail.com", Firstname: "Juan", Lastname: "C3", Location: &Location{State: "CA", Zip: "92129"}})
+	users = append(users, User{ID: "2", EncrPass: "secret",Email: "zakydfake@gmail.com", Firstname: "Zaky", Lastname: "Demnianiek"})
 	router.HandleFunc("/", handler)
 	router.HandleFunc("/people", getUsersEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", getUserEndpoint).Methods("GET")
@@ -67,11 +68,21 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to BagelBros CST service\n" +
-		"ZakyD, requests work like this:\n" +
-		"go to thisAddress/people to get a list of all users\n" +
-		"go to thisAddress/people/idnumber to get user information for someone with a certain id. up until now these have been get requests\n" +
-		"go to thisAddress/people/idnumber with a delete request to delete a certain user from the cache\n" +
-		"go to thisAddress/people/idnumber with a post request to post a user. the body should look like this (how it looks like when you do a get user request): {\"id\":\"69\" ....} \n" +
-		"KEEP IN MIND: this is not connected to the sql server yet so the information will get reset every time i rerun the program on my linux VM so the way of doing it might change for u in the future")
+	fmt.Fprint(w, `
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CST</title>
+</head>
+<h1>Welcome to the Coffee Shop Tutors website!</h1>
+<body>
+<p>ZakyD, requests work like this:</p>
+<p>go to thisAddress/people to get a list of all users</p>
+<p>go to thisAddress/people/idnumber to get user information for someone with a certain id. up until now these have been get requests</p>
+<p>go to thisAddress/people/idnumber with a delete request to delete a certain user from the cache</p>
+<p>go to thisAddress/people/idnumber with a post request to post a user. the body should look like this (how it looks like when you do a get user request): {\"id\":\"69\" ....}</p>
+<p>KEEP IN MIND: this is not connected to the sql server yet so the information will get reset every time i rerun the program on my linux VM so the way of doing it might change for u in the future")</p>
+</body>
+</html>`)
 }
