462af51f37b8ff2a3f92b3061b22fc8f96746818
Juan <juan@dreamvent.com>
2017-06-02 18:31:18 -0700

    error outputting

--- templates/newsql.go
+++ templates/newsql.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"fmt"
 )
 
 type User struct {
@@ -16,12 +17,12 @@ type User struct {
 }
 
 func isUniqueUser(u User){
-
+	//Todo: create a method for this
 }
 func DB() *sql.DB{
 	db, err := sql.Open("mysql", "root:Password!@/users")
 	if err != nil {
-		panic(err)
+		fmt.Print("could not access the database")
 	}
 	return db
 }
@@ -30,32 +31,41 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 
 		u := User{}
-
 		err := json.NewDecoder(r.Body).Decode(&u)
 		if err != nil {
-			panic(err)
+			w.WriteHeader(400)
+			w.Write([]byte("error in parsing user information. try again"))
+			return
 		}
 
 		db := DB()
+		defer db.Close()
+		//checking if email is already in database
+		var chkemail string
 		q := "SELECT * FROM users WHERE email=?"
-		rows, err := db.Query(q, u.Email)
+		err = db.QueryRow(q, u.Email).Scan(&chkemail)
 		if err != nil {
-			panic(err)
+			w.WriteHeader(400)
+			w.Write([]byte("error at check for duplicate email"))
+			return
 		}
-		defer rows.Close()
-
-		if rows.Next() {
-			//TODO: output some error
+		//dont take the input and recommend logging in with a forgot password button when
+		// the user enters a signup email that is the same as one in the database
+		if chkemail == u.Email {
+			w.WriteHeader(400)
+			w.Write([]byte("This email has already been used. Queue login?"))
+			return
 		}
 
 		q = "INSERT INTO users VALUES(?, ?, ?, ?, ?)"
 		_, err = db.Exec(q, u.ID, u.Email, u.EncrPass, u.Firstname, u.Lastname)
 		if err != nil {
-			//todo: respond with error (on the server side)
-			panic(err)
+			w.WriteHeader(400)
+			w.Write([]byte("error writing values in the "))
+
+			return
 		}
 		w.WriteHeader(200)
-		w.Write([]byte("gg"))
 	} else {
 		w.WriteHeader(400)
 		w.Write([]byte("Incorrect request type. Please do a post request"))
@@ -65,6 +75,7 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	//struct for body
 	//{"id": 189}
 	u := User{}
+	//creating stuct & json parse
 	type IdBody struct {
 		ID int `json:"id"`
 	}
@@ -72,19 +83,23 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&id)
 	db := DB()
 	defer db.Close()
+	//check post method
 	if r.Method == http.MethodPost {
+		//query, parse and encode
 		q:= "SELECT * FROM users WHERE id=?"
 		var uid, ue, up, uf, ul string
 		err  := db.QueryRow(q, id.ID).Scan(&uid, &ue, &up, &uf, &ul)
 		if err != nil {
+			w.WriteHeader(400)
 			w.Write([]byte("err at query"))
-			panic(err)
+			return
 		}
 		u.ID = string(uid)
 		u.Email = ue
 		u.EncrPass = up
 		u.Firstname =uf
 		u.Lastname = ul
+		w.WriteHeader(200)
 		json.NewEncoder(w).Encode(u)
 
 	} else {
