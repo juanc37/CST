2f67228c07649f4189da37d5f9d980661f7964ec
Juan <juan@dreamvent.com>
2017-06-11 12:22:09 -0700

    added planned fields to sessions

--- templates/sessionfuncs.go
+++ templates/sessionfuncs.go
@@ -8,10 +8,12 @@ import (
 )
 type Session struct{
 	ID       string `json:"id"`
-	time     string `json:"time"`
-	tutoree  string `json:"tutoreeID"`
-	tutor    string `josn:"tutorID"`
-
+	Tutoree  string `json:"tutoreeID"`
+	Tutor    string `josn:"tutorID"`
+	Time     string `json:"time"`
+	Location string `json:"location"`
+	Clockin  string `json:"clockin"`
+	Clockout string `json:"clockout"`
 }
 func sDB() *sql.DB{
 	db, err := sql.Open("mysql", "root:Password!@/session")
@@ -34,14 +36,55 @@ func addSession(w http.ResponseWriter, r *http.Request) {
 		db := sDB()
 		defer db.Close()
 		//enter the values in the database
-		q := "INSERT INTO sessions(time, tutorID, tutorID) VALUES(?, ?, ?)"
-		_, err = db.Exec(q, s.time, s.tutoree, s.tutor)
+		q := "INSERT INTO sessions(tutoreeID, tutorID, time, location) VALUES(?, ?, ?, ?)"
+		_, err = db.Exec(q, s.Tutoree, s.Tutor, s.Time, s.Location)
 		if err != nil {
 			w.WriteHeader(400)
 			w.Write([]byte("error when writing info to database. (incorrect format?) "))
 			return
 		}
 		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(400)
+		w.Write([]byte("Incorrect request type. Please do a post request"))
+	}
+}
+func getAllTutorSessions(w http.ResponseWriter, r *http.Request) {
+	//struct for body
+	//{"id": 189, "access":"secret code"}
+	s := Session{}
+	//creating stuct & json parse
+	type IdBody struct {
+		ID int `json:"id"`
+		Accesscode string `json:"access"`
+	}
+	bod := IdBody{}
+	err := json.NewDecoder(r.Body).Decode(&bod)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("error in parsing user information. try again"))
+		return
+	}
+	if bod.Accesscode != "wiener"{
+		w.WriteHeader(400)
+		w.Write([]byte("icorrect passcode to access users"))
+		return
+	}
+	db := uDB()
+	defer db.Close()
+	//check post method
+	if r.Method == http.MethodPost {
+		//query, parse and encode
+		q:= "SELECT * FROM sessions WHERE tutorID=?"
+		err  := db.QueryRow(q, bod.ID).Scan(&s.ID, &s.Tutoree, &s.Tutor, &s.Time, &s.Location, &s.Clockin, &s.Clockout)
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte("err at query : check ID field"))
+			return
+		}
+		w.WriteHeader(200)
+		json.NewEncoder(w).Encode(s)
+
 	} else {
 		w.WriteHeader(400)
 		w.Write([]byte("Incorrect request type. Please do a post request"))
@@ -74,7 +117,7 @@ func getSession(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		//query, parse and encode
 		q:= "SELECT * FROM sessions WHERE id=?"
-		err  := db.QueryRow(q, bod.ID).Scan(&s.ID, &s.time, &s.tutoree, &s.tutor)
+		err  := db.QueryRow(q, bod.ID).Scan(&s.ID, &s.Time, &s.Tutoree, &s.Tutor)
 		if err != nil {
 			w.WriteHeader(400)
 			w.Write([]byte("err at query : check ID field"))
